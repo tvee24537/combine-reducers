969118a0d9dcea7578fff8c44a75b5a188ef4775
Thanapat <t.veerakajorn@gmail.com>
2021-03-05 15:09:24 -0500

    manual submit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,50 @@
-export const addAuthor = author => {
-  return {
-    type: 'ADD_AUTHOR',
-    author
-  };
-};
+import { combineReducers } from "redux";
 
-export const removeAuthor = id => {
-  return {
-    type: 'REMOVE_AUTHOR',
-    id
-  };
-};
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
 
-export const addBook = book => {
-  return {
-    type: 'ADD_BOOK',
-    book
-  };
-};
+export default rootReducer;
 
-export const removeBook = id => {
-  return {
-    type: 'REMOVE_BOOK',
-    id
-  };
-};
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+
+    case "REMOVE_BOOK":
+      idx = state.findIndex(book => book.id === action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+} 
+
+import uuid from "uuid";
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(book => book.id === action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+    default:
+      return state;
+  }
+} 
--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,3 +1,11 @@
+import { createStore } from "redux";
+import rootReducer from "./reducers/manageAuthorsAndBooks";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); 
+
 export default function bookApp(
   state = {
     authors: [],
